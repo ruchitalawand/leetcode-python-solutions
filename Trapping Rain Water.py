b0c4a7394d2e9e2870c6f16e99a5a2cd1503dd64
Ruchita Lawand <122746052+ruchitalawand@users.noreply.github.com>
2025-10-09 21:36:33 +0530

    Solution of Trapping Rain Water

---
+++ Trapping Rain Water.py
@@ -0,0 +1,22 @@
+Trapping Rain Water
+
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        if not height:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += max(0, left_max - height[left])
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += max(0, right_max - height[right])
+        
+        return water
